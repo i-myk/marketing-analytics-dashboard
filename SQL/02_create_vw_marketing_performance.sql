3e1c311dae941e63f4b95964fb5d30c920b63fb1
Igor Mykoliv <83515477+i-myk@users.noreply.github.com>
2026-06-08 18:29:21 -0500

    Update 02_create_vw_marketing_performance.sql
    Add marketing performance view

--- SQL/02_create_vw_marketing_performance.sql
+++ SQL/02_create_vw_marketing_performance.sql
@@ -1,1 +1,25 @@
+CREATE OR REPLACE VIEW
+`fifth-flash-489402-h9.improvado_assignment.vw_marketing_performance`
+AS
+
+SELECT
+    date,
+    platform,
+    campaign_id,
+    campaign_name,
+    ad_group_id,
+    ad_group_name,
+    impressions,
+    clicks,
+    cost,
+    conversions,
+    conversion_value,
+
+    SAFE_DIVIDE(clicks, impressions) AS ctr,
+    SAFE_DIVIDE(cost, clicks) AS cpc,
+    SAFE_DIVIDE(cost, conversions) AS cpa,
+    SAFE_DIVIDE(conversions, clicks) AS conversion_rate,
+    SAFE_DIVIDE(conversion_value, cost) AS roas
+
+FROM `fifth-flash-489402-h9.improvado_assignment.ads_marketing_performance`;
 
